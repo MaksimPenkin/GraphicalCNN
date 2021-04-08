6bbe5f7129ae07dfc31710f645e1263e5df36ce9
MaksimPenkin <penkin97@gmail.com>
2021-04-08 22:35:02 +0300

    change welcome image

--- run.py
+++ run.py
@@ -35,7 +35,7 @@ class WelcomeFrame(BaseFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs) 
         self.width, self.height = 196, 196
-        self.image = Image.open("./imgs/welcome.jpg")
+        self.image = Image.open("./imgs/welcome.png")
         self.createWidgets()
     
     def createWidgets(self):
