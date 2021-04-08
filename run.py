eb4ec4122b6e42264fa7f5636abeb27fa471dcf9
MaksimPenkin <penkin97@gmail.com>
2021-04-08 22:50:27 +0300

    добавлены заглушки под различные окна меню

--- run.py
+++ run.py
@@ -63,9 +63,9 @@ class MainFrame(BaseGridFrame):
         self.createWidgets()
 
     def createWidgets(self):
-        self.settingButton = tk.Button(self, text='Settings', command=None)
-        self.visualButton = tk.Button(self, text='Visualization', command=None)
-        self.logsButton = tk.Button(self, text='Logs', command=None)
+        self.settingButton = tk.Button(self, text='Settings', command=self.get_settingFrame)
+        self.visualButton = tk.Button(self, text='Visualization', command=self.get_visualFrame)
+        self.logsButton = tk.Button(self, text='Logs', command=self.get_logsFrame)
         self.quitButton = tk.Button(self, text='Quit', command=self.master.quit)
 
         self.workingFrame = WelcomeFrame(self)
@@ -75,6 +75,64 @@ class MainFrame(BaseGridFrame):
         self.logsButton.grid(row=0, column=2, sticky="NEWS")
         self.workingFrame.grid(row=1, column=0, columnspan=3, sticky="NEWS")
         self.quitButton.grid(row=2, column=2, sticky="NEWS")
+
+    def update_workingFrame(self):
+        self.workingFrame.grid_forget()
+        self.workingFrame.grid(row=1, column=0, columnspan=3, sticky="NEWS")
+    
+    def get_settingFrame(self):
+        self.workingFrame = SettingFrame(self)
+        self.update_workingFrame()
+    
+    def get_visualFrame(self):
+        self.workingFrame = VisualFrame(self)
+        self.update_workingFrame()
+
+    def get_logsFrame(self):
+        self.workingFrame = LogsFrame(self)
+        self.update_workingFrame()
+
+
+class SettingFrame(BaseGridFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)        
+        for i in range(3):
+            self.rowconfigure(i, weight=1)
+        for j in range(3):
+            self.columnconfigure(j, weight=1)
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.label = tk.Label(self, text='TODO Setting Frame')
+        self.label.grid(row=0, column=0, sticky="NEWS")
+
+
+class VisualFrame(BaseGridFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)        
+        for i in range(3):
+            self.rowconfigure(i, weight=1)
+        for j in range(3):
+            self.columnconfigure(j, weight=1)
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.label = tk.Label(self, text='TODO Visual Frame')
+        self.label.grid(row=0, column=0, sticky="NEWS")
+
+
+class LogsFrame(BaseGridFrame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)        
+        for i in range(3):
+            self.rowconfigure(i, weight=1)
+        for j in range(3):
+            self.columnconfigure(j, weight=1)
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.label = tk.Label(self, text='TODO Logs Frame')
+        self.label.grid(row=0, column=0, sticky="NEWS")
 
 
 if __name__ == "__main__":
