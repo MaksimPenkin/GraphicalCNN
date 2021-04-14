2fb429fb0c81e7cc7e2644d4c383c1bf91c3ffa9
MaksimPenkin <penkin97@gmail.com>
2021-04-14 18:15:10 +0300

    генератор добавлен как основная модель сегментации

---
+++ neural/models.py
@@ -0,0 +1,41 @@
+""" 
+ @author   Maksim Penkin @MaksimPenkin
+ @author   Oleg Khokhlov @okhokhlov
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from neural.blocks.embedding import Embedding
+from neural.blocks.encoder import EncoderResBlock, EncoderConvBlock
+from neural.blocks.decoder import DecoderResBlock, DecoderConvBlock
+from neural.blocks.restoration import Restoration
+
+
+class Generator(nn.Module):
+    def __init__(self, num_filters, num_blocks=4, batch_norm=False, arch='ResBlock'):
+        super().__init__()
+        
+        self.num_filters = num_filters
+        self.num_blocks = num_blocks
+        self.batch_norm = batch_norm
+        self.arch = arch
+
+        self.emb = Embedding(3, self.num_filters, self.batch_norm)
+        if self.arch == 'ResBlock':
+            self.encoder = EncoderResBlock(self.num_filters, self.num_blocks, self.batch_norm)
+            self.decoder = DecoderResBlock(self.num_filters, self.num_blocks, self.batch_norm)
+        elif self.arch == 'ConvBlock':
+            self.encoder = EncoderConvBlock(self.num_filters, self.num_blocks, self.batch_norm)
+            self.decoder = DecoderConvBlock(self.num_filters, self.num_blocks, self.batch_norm)
+        self.restore = Restoration(self.num_filters, 2, self.batch_norm)
+
+    def forward(self, x):
+        x_emb = self.emb(x)
+        encoder_acts = self.encoder(x_emb)
+        y = self.decoder(encoder_acts)
+        y = self.restore(y)
+
+        return y
+
+
