14a5e9a1090eea104a14830908e9be1196b8c0b1
Oleg Khokhlov <olegish@gmail.com>
2021-04-15 14:44:49 +0300

    add new dataset

---
+++ neural/dataset/paired_dataset.py
@@ -0,0 +1,44 @@
+""" 
+ @author   Maksim Penkin @MaksimPenkin
+ @author   Oleg Khokhlov @okhokhlov
+"""
+
+import os
+import numpy as np
+import pandas as pd  
+import cv2 
+
+import torch
+from torch.utils.data import Dataset
+
+
+class SegmDataset(Dataset):
+    def __init__(self, csv_file, root_dir, transform=None):
+        self.csv_data = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.csv_data)
+
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.root_dir,
+                                self.csv_data.iloc[idx, 0])
+        mask_name = os.path.join(self.root_dir,
+                                self.csv_data.iloc[idx, 1])
+        
+        image = cv2.imread(img_name)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = image.astype(np.float32) / 255.
+        
+        mask = cv2.imread(mask_name, 0).astype(np.float32) / 255.
+        mask = mask[...,np.newaxis]
+        
+        sample = {'image': image, 'mask': mask}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+
