4d43a72f53f002d04671e7333b291e197da3e4b7
Oleg Khokhlov <olegish@gmail.com>
2021-04-30 14:00:24 +0300

    add more tests

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-from neural.blocks.layers import conv3x3, ResBlock
+from neural.blocks.layers import conv3x3, ResBlock, ConvBlock, UpSample, conv5x5
 import torch
 
 
@@ -19,6 +19,28 @@ class TestNeuralBlocks(unittest.TestCase):
         tensor = torch.randn(8, 16, 224, 224)
 
         self.assertSequenceEqual(block(tensor).shape, (8, 16, 224, 224))
+
+    def test_conv_5x5_bloc(self):
+        """Test conv block"""
+        block = conv5x5(3, 64)
+        tensor = torch.randn(8, 3, 224, 224)
+
+        self.assertSequenceEqual(block(tensor).shape, (8, 64, 224, 224))
+
+    def test_conv_bloc(self):
+        """Test conv block"""
+        block = ConvBlock(16)
+        tensor = torch.randn(8, 16, 224, 224)
+
+        self.assertSequenceEqual(block(tensor).shape, (8, 16, 224, 224))
+
+    def test_up_sample_bloc(self):
+        """Test conv block"""
+        block = UpSample(3)
+        tensor_1 = torch.randn(8, 3, 112, 112)
+        tensor_2 = torch.randn(8, 3, 224, 224)
+
+        self.assertSequenceEqual(block(tensor_1, tensor_2).shape, (8, 3, 224, 224))
 
 
 if __name__ == "__main__":
